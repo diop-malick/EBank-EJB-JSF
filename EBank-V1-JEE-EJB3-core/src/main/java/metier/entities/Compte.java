f18441c45d6b134ca1dddeb2ea1625f1bc0c4c22
Malick <diop-malick@hotmail.fr>
2016-03-28 04:04:46 +0200

    simplifie jboss maven dependecny

---
+++ EBank-V1-JEE-EJB3-core/src/main/java/metier/entities/Compte.java
@@ -0,0 +1,128 @@
+package metier.entities;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Date;
+
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.UniqueConstraint;
+
+
+@Entity
+@Table(name = "ebank_compte",uniqueConstraints = {@UniqueConstraint(columnNames={"numeroCompte"})})
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "TYPE_CPTE", discriminatorType = DiscriminatorType.STRING, length = 2)
+public abstract class Compte implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	/* properties */
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String numeroCompte;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date dateCreation;
+	
+	private double solde;
+	
+	private boolean active;
+	
+	@ManyToOne
+	@JoinColumn(name = "id_client")
+	private Client client;
+
+	@ManyToOne
+	@JoinColumn(name = "id_employe")
+	private Employe employe;
+
+	@OneToMany(mappedBy = "compte")
+	private Collection<Operation> operations;
+
+	// constructor 
+	
+	public Compte() {
+	}
+
+	// getters and setters 
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNumeroCompte() {
+		return numeroCompte;
+	}
+
+	public void setNumeroCompte(String numeroCompte) {
+		this.numeroCompte = numeroCompte;
+	}
+
+	public Date getDateCreation() {
+		return dateCreation;
+	}
+
+	public void setDateCreation(Date dateCreation) {
+		this.dateCreation = dateCreation;
+	}
+
+	public double getSolde() {
+		return solde;
+	}
+
+	public void setSolde(double solde) {
+		this.solde = solde;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	public Collection<Operation> getOperations() {
+		return operations;
+	}
+
+	public void setOperations(Collection<Operation> operations) {
+		this.operations = operations;
+	}
+
+	public Employe getEmploye() {
+		return employe;
+	}
+
+	public void setEmploye(Employe employe) {
+		this.employe = employe;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+}
